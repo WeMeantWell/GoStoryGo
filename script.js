3595d22cbb558b2db2eef4fbd34f829c7ce55b36
WeMeantWell <fredcallender@gmail.com>
2025-06-14 21:42:04 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 
 setTimeout(() => {
-  document.querySelector('.title').classList.remove('hidden');
-  document.querySelector('.title').classList.add('visible');
-}, 4700);
+  const book = document.getElementById("book");
+  book.src = "img/book_open.png";
+}, 3000);
+
+setTimeout(() => {
+  document.getElementById("splash").classList.remove("hidden");
+  document.getElementById("splash").classList.add("visible");
+}, 4200);
+
+setTimeout(() => {
+  document.getElementById("title").classList.remove("hidden");
+  document.getElementById("title").classList.add("visible");
+}, 4800);
 
 setTimeout(() => {
   window.location.href = "index.html";
