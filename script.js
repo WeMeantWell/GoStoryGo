6ab8669e20b9bf1735e37afc064a71fe24f79f45
WeMeantWell <fredcallender@gmail.com>
2025-06-15 04:23:09 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// JS animation and redirect logic
+
+window.onload = function() {
+    const tagline = "You have found the easiest-to-use, most fun way to write, illustrate, and publish your own children's books!";
+    let i = 0;
+    function typeWriter() {
+        if (i < tagline.length) {
+            document.getElementById("tagline").innerHTML += tagline.charAt(i);
+            i++;
+            setTimeout(typeWriter, 40);
+        }
+    }
+    setTimeout(() => {
+        typeWriter();
+        setTimeout(() => {
+            window.location.href = "main.html";
+        }, 5000);
+    }, 1500);
+};
