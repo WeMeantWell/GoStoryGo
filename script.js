d704168767cdcd4a617bbfea10ceae36abb3bb57
WeMeantWell <fredcallender@gmail.com>
2025-06-15 01:50:01 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const tagline = document.getElementById('tagline');
-  tagline.textContent = "You have found the easiest-to-use, most fun way to write, illustrate, and publish your own children's books!";
-  setTimeout(() => {
-    window.location.href = "main.html";
-  }, 8000);
-});
+
+setTimeout(() => {
+  window.location.href = "main.html";
+}, 7000);
